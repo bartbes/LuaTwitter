f03fd1aa123413b58699c12cdca942989d92264e
Linus Sjögren <thelinx@unreliablepollution.net>
2009-09-22 18:14:30 +0200

    Added some error checking, edited return values

--- luatwitter.lua
+++ luatwitter.lua
@@ -7,7 +7,7 @@ local print = print
 
 module("twitter")
 
-local request = [[
+local tweetheaders = [[
 POST %s HTTP/1.0
 Host: twitter.com
 Authorization: Basic %s
@@ -17,10 +17,20 @@ Content-length: %d
 %s
 ]]
 
+--- Updates a user's status on Twitter.
+-- @param message The message to tweet.
+-- @param user The user to tweet as -OR- the mime64 encoded auth string.
+-- @param pass If specified it will use user:pass authentication.
 function updatestatus(message, user, pass)
-	local auth = mime.b64(user .. ":" .. pass)
+    if not message then return false end
+    local auth
+    if not pass then
+        auth = user
+    else
+	    auth = mime.b64(user .. ":" .. pass)
+    end
 	local postdata = "status=" .. url.escape(message)
-	local data = request:format("/statuses/update.json", auth, #postdata, postdata)
+	local data = tweetheaders:format("/statuses/update.json", auth, #postdata, postdata)
 	local sock = socket.tcp()
 	sock:settimeout(15)
 	sock:connect("twitter.com", 80)
@@ -29,5 +39,6 @@ function updatestatus(message, user, pass)
 	sock:close()
 	response = response:match(".-\r\n\r\n(.*)")
 	response = json.decode(response)
-	print("Tweeted: " .. response.text)
+	--print("Tweeted: " .. response.text)
+	return true, response
 end
